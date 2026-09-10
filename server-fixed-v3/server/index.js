aea8fec0d7430031c5a819bf87da84db6de95c46
VIPUL-2711 <146448858+VIPUL-2711@users.noreply.github.com>
2026-09-10 21:58:00 +0530

    Update index.js

--- server-fixed-v3/server/index.js
+++ server-fixed-v3/server/index.js
@@ -8,25 +8,21 @@ const courseRoutes = require("./routes/Course");
 
 const database =require("./config/database");
 const cookieParser=require("cookie-parser");
-const cors=require("cors");
 const {cloudinaryConnect}=require("./config/cloudinary")
 const fileUpload =require("express-fileupload");
 const dotenv=require("dotenv");
 
 dotenv.config();
 const PORT =process.env.PORT || 4000;
-
+app.use(cors({
+  origin: "https://studynotion-ed-tech-1-9bsy.onrender.com",
+  credentials: true
+}));
 //database
 database.connect();
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin:"http://localhost:3000",
-    credentials:true,
-  })
-)
 app.use(
   fileUpload({
     useTempFiles:true,
